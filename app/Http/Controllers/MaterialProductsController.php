0c847eed08d2369b028ebb3cfc2273dd04968be3
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-07-15 10:43:33 +0530

    dso changes

--- app/Http/Controllers/MaterialProductsController.php
+++ app/Http/Controllers/MaterialProductsController.php
@@ -245,11 +245,10 @@ class MaterialProductsController extends Controller
                     'require_outlife_tracking'     =>  $row['require_outlife_tracking'],
                     'outlife'                      => $row['outlife'] ?? null,
                     'storage_area'                 => $storage_area->id,
+                    'disposed_after'  => $row['date_of_disposal']?? NULL,
                     'housing_type'                 => $housing_type->id,
                     'housing'                      => $row['housing'] == '-' ? 'nil' : $row['housing'],
                     'department'                   => $department->id,
-                    'disposed_after'  => 
-                    $row['date_of_disposal']?? NULL,
                     'access'                       => $row['access'] ?? null,
                     'date_in'                      => strExcelDate($row['date_in']),
                     'date_of_expiry'               => strExcelDate($row['date_of_expiry']),
